bf3e0dabdf3a8b922847835fba16bd9cc6be2b93
vyut <vyut@msn.com>
2025-02-28 15:19:36 +0700

    Install dotenv and Update server.ts file

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,26 @@
 import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+import authorRoutes from './routes/authorRoute';
+import bookRoutes from './routes/bookRoute';
+import memberRoutes from './routes/memberRoute';
+import borrowingRoutes from './routes/borrowingRoute';
+
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+app.use('/authors', authorRoutes);
+app.use('/books', bookRoutes);
+app.use('/members', memberRoutes);
+app.use('/borrowings', borrowingRoutes);
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!');
 });
 
-app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`App listening at http://localhost:${PORT}`);
 });
